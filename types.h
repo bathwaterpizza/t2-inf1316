ce86d0d9249a8fdaa4480bb15a2374ab5b4306a6
bathwaterpizza <cakto002@gmail.com>
2024-11-17 18:42:58 -0300

    Initial files

---
+++ types.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define PIPE_READ 0
+#define PIPE_WRITE 1
+
+#define SEM_P1_NAME "/sem_P1"
+#define SEM_P2_NAME "/sem_P2"
+#define SEM_P3_NAME "/sem_P3"
+#define SEM_P4_NAME "/sem_P4"
---
+++ vmem_sim.c
@@ -0,0 +1,64 @@
+#include "types.h"
+#include <fcntl.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+  // create one pipe for each process,
+  // these are used to send the page op requests
+  int pipe_P1[2], pipe_P2[2], pipe_P3[2], pipe_P4[2];
+
+  if (pipe(pipe_P1) == -1) {
+    perror("Pipe error");
+    exit(1);
+  }
+  if (pipe(pipe_P2) == -1) {
+    perror("Pipe error");
+    exit(1);
+  }
+  if (pipe(pipe_P3) == -1) {
+    perror("Pipe error");
+    exit(1);
+  }
+  if (pipe(pipe_P4) == -1) {
+    perror("Pipe error");
+    exit(1);
+  }
+
+  // create one semaphore for each process,
+  // these are used to tell a process when to send a page op request
+  sem_t *sem_P1, *sem_P2, *sem_P3, *sem_P4;
+
+  sem_unlink(SEM_P1_NAME);
+  sem_unlink(SEM_P2_NAME);
+  sem_unlink(SEM_P3_NAME);
+  sem_unlink(SEM_P4_NAME);
+
+  sem_P1 = sem_open(SEM_P1_NAME, O_CREAT, 0666, 0);
+  if (sem_P1 == SEM_FAILED) {
+    perror("sem_open(SEM_P1_NAME) failed");
+    exit(2);
+  }
+  sem_P2 = sem_open(SEM_P2_NAME, O_CREAT, 0666, 0);
+  if (sem_P2 == SEM_FAILED) {
+    perror("sem_open(SEM_P2_NAME) failed");
+    exit(2);
+  }
+  sem_P3 = sem_open(SEM_P3_NAME, O_CREAT, 0666, 0);
+  if (sem_P3 == SEM_FAILED) {
+    perror("sem_open(SEM_P3_NAME) failed");
+    exit(2);
+  }
+  sem_P4 = sem_open(SEM_P4_NAME, O_CREAT, 0666, 0);
+  if (sem_P4 == SEM_FAILED) {
+    perror("sem_open(SEM_P4_NAME) failed");
+    exit(2);
+  }
+
+  // cleanup
+
+  return 0;
+}
