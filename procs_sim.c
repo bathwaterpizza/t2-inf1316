357fe768d2f14718dea0afb47c2a63bdab2c66f8
bathwaterpizza <cakto002@gmail.com>
2024-11-17 19:09:39 -0300

    Created semaphores and pipes

--- procs_sim.c
+++ procs_sim.c
@@ -1,6 +1,70 @@
 #include "types.h"
+#include <fcntl.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-  // a
+  // open a pipe for each process, to receive memory io requests
+  int pipe_P1[2], pipe_P2[2], pipe_P3[2], pipe_P4[2];
+
+  if (pipe(pipe_P1) == -1) {
+    perror("Pipe error");
+    exit(1);
+  }
+  if (pipe(pipe_P2) == -1) {
+    perror("Pipe error");
+    exit(1);
+  }
+  if (pipe(pipe_P3) == -1) {
+    perror("Pipe error");
+    exit(1);
+  }
+  if (pipe(pipe_P4) == -1) {
+    perror("Pipe error");
+    exit(1);
+  }
+
+  // create a semaphore for each process, to sync requests from processes
+  sem_t *sem_P1, *sem_P2, *sem_P3, *sem_P4;
+
+  sem_unlink(SEM_P1_NAME);
+  sem_unlink(SEM_P2_NAME);
+  sem_unlink(SEM_P3_NAME);
+  sem_unlink(SEM_P4_NAME);
+
+  sem_P1 = sem_open(SEM_P1_NAME, O_CREAT, 0666, 0);
+  if (sem_P1 == SEM_FAILED) {
+    perror("Sem error");
+    exit(2);
+  }
+  sem_P2 = sem_open(SEM_P2_NAME, O_CREAT, 0666, 0);
+  if (sem_P2 == SEM_FAILED) {
+    perror("Sem error");
+    exit(2);
+  }
+  sem_P3 = sem_open(SEM_P3_NAME, O_CREAT, 0666, 0);
+  if (sem_P3 == SEM_FAILED) {
+    perror("Sem error");
+    exit(2);
+  }
+  sem_P4 = sem_open(SEM_P4_NAME, O_CREAT, 0666, 0);
+  if (sem_P4 == SEM_FAILED) {
+    perror("Sem error");
+    exit(2);
+  }
+
+  // cleanup
+  sem_close(sem_P1);
+  sem_close(sem_P2);
+  sem_close(sem_P3);
+  sem_close(sem_P4);
+  sem_unlink(SEM_P1_NAME);
+  sem_unlink(SEM_P2_NAME);
+  sem_unlink(SEM_P3_NAME);
+  sem_unlink(SEM_P4_NAME);
+
   return 0;
 }
