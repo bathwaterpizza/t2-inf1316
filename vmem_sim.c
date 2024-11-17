e80a887cd5a777a64114d15fdc802f393e911228
bathwaterpizza <cakto002@gmail.com>
2024-11-17 19:12:42 -0300

    Moved code to the correct source file..

--- vmem_sim.c
+++ vmem_sim.c
@@ -3,12 +3,11 @@
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-  // create one pipe for each process,
-  // these are used to send the page op requests
+  // open a pipe for each process, to receive memory io requests
   int pipe_P1[2], pipe_P2[2], pipe_P3[2], pipe_P4[2];
 
   if (pipe(pipe_P1) == -1) {
@@ -28,8 +27,7 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  // create one semaphore for each process,
-  // these are used to tell a process when to send a page op request
+  // create a semaphore for each process, to sync requests from processes
   sem_t *sem_P1, *sem_P2, *sem_P3, *sem_P4;
 
   sem_unlink(SEM_P1_NAME);
@@ -39,26 +37,34 @@ int main(int argc, char **argv) {
 
   sem_P1 = sem_open(SEM_P1_NAME, O_CREAT, 0666, 0);
   if (sem_P1 == SEM_FAILED) {
-    perror("sem_open(SEM_P1_NAME) failed");
+    perror("Sem error");
     exit(2);
   }
   sem_P2 = sem_open(SEM_P2_NAME, O_CREAT, 0666, 0);
   if (sem_P2 == SEM_FAILED) {
-    perror("sem_open(SEM_P2_NAME) failed");
+    perror("Sem error");
     exit(2);
   }
   sem_P3 = sem_open(SEM_P3_NAME, O_CREAT, 0666, 0);
   if (sem_P3 == SEM_FAILED) {
-    perror("sem_open(SEM_P3_NAME) failed");
+    perror("Sem error");
     exit(2);
   }
   sem_P4 = sem_open(SEM_P4_NAME, O_CREAT, 0666, 0);
   if (sem_P4 == SEM_FAILED) {
-    perror("sem_open(SEM_P4_NAME) failed");
+    perror("Sem error");
     exit(2);
   }
 
   // cleanup
+  sem_close(sem_P1);
+  sem_close(sem_P2);
+  sem_close(sem_P3);
+  sem_close(sem_P4);
+  sem_unlink(SEM_P1_NAME);
+  sem_unlink(SEM_P2_NAME);
+  sem_unlink(SEM_P3_NAME);
+  sem_unlink(SEM_P4_NAME);
 
   return 0;
 }
